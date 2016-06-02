4af0df1bdb665e9e3032722072a89ccca4a856a8
Guilherme Siquinelli <guiseek@gmail.com>
2016-06-01 22:20:27 -0300

    Atualizaçoes da segunda aula

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 angular
-    .module('App',[])
-    .controller('AppController', function($scope) {
-       $scope.categorias = [
-           { nome: "Categoria 1" },
-           { nome: "Categoria 2" },
-           { nome: "Categoria 3" },
-           { nome: "Categoria 4" },
-           { nome: "Categoria 5" }
-       ];
-       $scope.cervejas = [
-           { nome: "Eisenbahn", volume: "8" },
-           { nome: "Gebenha", volume: "7" },
-           { nome: "Brahma", volume: "5" }
-       ];
-       $scope.cadastrarCerveja = function(cerveja) {
-           $scope.cervejas.push(cerveja)
-       }
-    });
+  .module('App', [])
+  .controller('AppController', function ($scope) {
+    $scope.categorias = [
+      { nome: "Categoria 1" },
+      { nome: "Categoria 2" },
+      { nome: "Categoria 3" },
+      { nome: "Categoria 4" },
+      { nome: "Categoria 5" }
+    ];
+    $scope.cervejas = [
+      { nome: "Eisenbahn", volume: 8 },
+      { nome: "Gebenha", volume: 7 },
+      { nome: "Brahma", volume: 5 }
+    ];
+    $scope.salvarCerveja = function (cerveja) {
+      var novaCerveja = angular.copy(cerveja);
+      if (cerveja.id) {
+        $scope.cervejas[cerveja.id] = novaCerveja;
+      } else {
+        $scope.cervejas.push(novaCerveja)
+      }
+      $scope.cerveja = {};
+    }
+    $scope.popularForm = function ($index, cerveja) {
+      cerveja.id = $index;
+      $scope.cerveja = angular.copy(cerveja);
+    }
+    $scope.deletarCerveja = function ($index, cerveja) {
+      if (confirm("Tem certeza que deseja remover a cerveja: " + cerveja.nome + "?")) {
+        $scope.cervejas.splice($index, 1);
+      }
+    }
+  });
