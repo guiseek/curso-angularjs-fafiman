ddf0166b089f09275f48acfd27f03a5c7be68ab7
Guilherme Siquinelli <guiseek@gmail.com>
2016-06-02 22:23:04 -0300

    Curso AngularJS Fafiman Fim

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 angular
-  .module('App', [])
+  .module('App', ['ngRoute'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home.html',
+        controller: 'HomeController'
+      })
+      .when('/cervejaria', {
+        templateUrl: 'cervejaria/index.html',
+        controller: 'CervejariaController'
+      })
+  })
   .controller('AppController', function ($scope) {
     $scope.categorias = [
       { nome: "Categoria 1" },
@@ -8,27 +19,4 @@ angular
       { nome: "Categoria 4" },
       { nome: "Categoria 5" }
     ];
-    $scope.cervejas = [
-      { nome: "Eisenbahn", volume: 8 },
-      { nome: "Gebenha", volume: 7 },
-      { nome: "Brahma", volume: 5 }
-    ];
-    $scope.salvarCerveja = function (cerveja) {
-      var novaCerveja = angular.copy(cerveja);
-      if (cerveja.id) {
-        $scope.cervejas[cerveja.id] = novaCerveja;
-      } else {
-        $scope.cervejas.push(novaCerveja)
-      }
-      $scope.cerveja = {};
-    }
-    $scope.popularForm = function ($index, cerveja) {
-      cerveja.id = $index;
-      $scope.cerveja = angular.copy(cerveja);
-    }
-    $scope.deletarCerveja = function ($index, cerveja) {
-      if (confirm("Tem certeza que deseja remover a cerveja: " + cerveja.nome + "?")) {
-        $scope.cervejas.splice($index, 1);
-      }
-    }
   });
